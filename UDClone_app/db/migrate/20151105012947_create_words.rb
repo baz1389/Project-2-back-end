352b078eeccd0f0f443e0560036fd63c525b495a
Ben <benjamin.a.spencer@gmail.com>
2015-11-04 20:36:57 -0500

    Replaced the sample_sentence value form string to text.
    Changes confirmed in psql console.

--- UDClone_app/db/migrate/20151105012947_create_words.rb
+++ UDClone_app/db/migrate/20151105012947_create_words.rb
@@ -2,7 +2,7 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
       t.string :definition
-      t.string :sample_sentence
+      t.text :sample_sentence
 
       t.timestamps null: false
     end
--- UDClone_app/db/schema.rb
+++ UDClone_app/db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20151105012947) do
 
   create_table "words", force: :cascade do |t|
     t.string   "definition"
-    t.string   "sample_sentence"
+    t.text     "sample_sentence"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
